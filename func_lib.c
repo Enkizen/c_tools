d32724afbc627e5d00fb7d231e52bc4924b96238
Enkizen <chrono.dreamz@gmail.com>
2019-08-07 21:51:52 +0800

    Create func_lib.c for future uses

---
+++ func_lib.c
@@ -0,0 +1,9 @@
+/* This file serves as a library for tools that will speed coding up.
+ * It is uncomfortable to program in C after using Java and Javascript for so long.
+ * My favourite functions like map and filter are all gone.
+ * Maybe it is time to build them myself to gain mastery of C.
+ */
+ 
+ #include "func_lib.h"  /* Include the header (not strictly necessary here) */
+
+/* Function definition */
